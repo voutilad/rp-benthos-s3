29c845b3b4d6f5e0f60c81d307a262714ae365e1
Dave Voutila <voutilad@gmail.com>
2024-05-28 15:28:35 -0400

    add sample data and config for cracking tgzs

---
+++ Makefile
@@ -0,0 +1,7 @@
+.PHONY: clean samples
+
+samples:
+	make -C samples
+
+clean:
+	make -C samples clean
--- api_download.sh
+++ api_download.sh
@@ -2,5 +2,5 @@
 
 curl \
     -H "Authorization: Bearer $(cat auth/access_token)" \
-    https://api.enterprise.wikimedia.com/v2/snapshots/enwiki_namespace_0/download \
+    -L https://api.enterprise.wikimedia.com/v2/snapshots/enwiki_namespace_0/download \
     --output enwiki.tar.gz
