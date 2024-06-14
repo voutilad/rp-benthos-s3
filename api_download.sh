1c87b75200e1c840556e59f4d7e42d73bded1f23
Dave Voutila <voutilad@gmail.com>
2024-06-14 10:55:27 -0400

    nuke some old wikimedia api stuff

--- api_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl \
-    -H "Authorization: Bearer $(cat auth/access_token)" \
-    -L https://api.enterprise.wikimedia.com/v2/snapshots/enwiki_namespace_0/download \
-    --output enwiki.tar.gz
--- api_snapshots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl \
-    -X POST \
-    -H "Authorization: Bearer $(cat auth/access_token)" \
-    -d '{"fields": "[\"name\",\"identifier\"]","filters": "[{\"field\":\"namespace.identifier\",\"value\":0}]"}' \
-    -L https://api.enterprise.wikimedia.com/v2/projects
